f82bf943fde7f58f2fc3f1449bed3e8bd64d4890
Richard Carp <richard.carp@clinithink.com>
2019-12-11 14:15:10 +0000

    Day 1, part 2 solution.

--- day01/main.py
+++ day01/main.py
@@ -3,6 +3,16 @@ def fuel_calc(i):
     return int(i / 3) - 2
 
 
+def fuel_calc_doubley_checked(i):
+    """Part 2"""
+    fuel = fuel_calc(i)
+
+    if fuel < 0:
+        return 0
+    else:
+        return fuel + fuel_calc_doubley_checked(fuel)
+
+
 if __name__ == "__main__":
     # Provided examples.
     assert fuel_calc(12) == 2
@@ -13,4 +23,9 @@ if __name__ == "__main__":
     # YOLO file path.
     inputs = [int(s) for s in open("input").readlines()]
 
+    ## Part 1
     print(sum(fuel_calc(i) for i in inputs))
+
+    ## Part 2.
+    print(sum(fuel_calc_doubley_checked(i) for i in inputs))
+
